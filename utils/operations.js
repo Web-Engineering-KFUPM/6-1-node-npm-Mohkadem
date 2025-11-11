a089a53de211f09c1fedc3866c57aef14aca1333
Mohkadem <s202245820@kfupm.edu.sa>
2025-11-11 16:16:58 +0300

    Complete the calculator & operations

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,26 @@
+import { result } from "lodash";
+
 export function add(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+  if (numbers.length === 0) return 0;
+  return numbers.slice(1).reduce((sum, num) => sum - num, numbers[0]);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  if (numbers.length === 0) return 0;
+  return numbers.reduce((res, num) => res * num, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
+  if (numbers.length === 0) return 0;
+  return numbers.slice(1).reduce((res, num) => {
+    if (num === 0) {
+      console.log("Error: Division by zero!");
+      process.exit(1);
+    }
+    return res / num;
+  }, numbers[0]);
 }
-
